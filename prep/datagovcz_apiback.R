6e7fdd1724133fc04bb7b32c258e872ac5726f0d
Petr Bouchal <pbouchal@gmail.com>
2020-01-31 14:32:39 +0100

    background code for accessing NKOD

---
+++ prep/datagovcz_apiback.R
@@ -0,0 +1,64 @@
+library(httr)
+library(tidyverse)
+library(jsonlite)
+
+ss <- GET("https://data.gov.cz/zdroj/lok%C3%A1ln%C3%AD-katalogy/CSttstckyU/214608232", accept_json()) %>%
+  content()
+tt <- ss[[1]][[6]]
+urls <- map_chr(tt, 2)
+
+nn <- urls %>% map(~GET(.x, accept_json()) %>% content(auto_unbox = T))
+names(nn[[1]][[1]])
+
+nn[[1]][[1]]$`http://www.w3.org/ns/dcat#keyword` %>%
+  map(`[[`, 'value') %>%
+  map_chr(1)
+nn[[1]][[1]]$`http://www.w3.org/ns/dcat#distribution` %>%
+  map(`[[`, 'value') %>%
+  map_chr(1)
+
+nn %>% map(names)
+innams <- nn[[1]] %>% map_dfc(names)
+
+titles <- nn %>% map(1) %>%
+  map(`[[`, "http://purl.org/dc/terms/title") %>%
+  map(1) %>%
+  map_chr(`[[`, "value")
+
+dist <- GET(nn[[9]][[1]]$`http://www.w3.org/ns/dcat#distribution`[[1]]$value, accept_json()) %>%
+  content()
+
+GET("https://data.gov.cz/api/v1/resource/distribution?iri=https://data.gov.cz/zdroj/datov%C3%A9-sady/http---vdb.czso.cz-pll-eweb-package_show-id-110080/distribuce/ca24b1ef468d717e0d8f14fa366e1a12",
+    accept_json()) %>%
+  content() %>%
+  pluck("jsonld", 2) %>%
+  pluck("http://www.w3.org/ns/dcat#downloadURL", 1, "@id")
+
+
+ss <- GET("https://data.gov.cz/zdroj/lok%C3%A1ln%C3%AD-katalogy/CSttstckyU/214608232", accept_json()) %>%
+  content()
+tt <- ss[[1]][[6]]
+urls <- map_chr(tt, 2)
+
+nn <- urls[400] %>% map(~GET(.x, accept_json()) %>% content(auto_unbox = T))
+
+zzz <- nn %>%
+  pluck(1) %>%
+  purrr::set_names(str_extract, "[:alnum:]*$") %>%
+  map(~purrr::set_names(.x, str_extract, "[:alnum:]*$")) %>%
+  pluck(1) %>%
+  map_dfc(~pluck(.x, 1, "value"))
+
+
+
+ddd <- GET("https://data.gov.cz/zdroj/datov%C3%A9-sady/http---vdb.czso.cz-pll-eweb-package_show-id-110080/distribuce/ca24b1ef468d717e0d8f14fa366e1a12",
+           accept_json()) %>%
+  content()
+
+ddd %>%
+  pluck(1) %>%
+  map_df(~pluck(.x, 1, "value")) %>%
+  purrr::set_names(str_extract, "[:alnum:]*$")
+
+
+
