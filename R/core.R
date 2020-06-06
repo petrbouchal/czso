5ea8fa247281779e63f7825568826d817ce8ef10
Petr Bouchal <pbouchal@gmail.com>
2020-06-06 13:06:36 +0200

    improve documentation

--- R/core.R
+++ R/core.R
@@ -418,10 +418,10 @@ get_table <- function(dataset_id, resource_num = 1, force_redownload = FALSE) {
 #' You can pass this number to the function (even as a string), or you can pass the dataset ID found
 #' in the catalogue; the latter will have the form of e.g. `"cisNN"`.
 #'
-#' ### Relationships between codelists
+#' ### Relationships between codelists ("vazba mezi číselníky")
 #'
 #' The CZSO data store also holds tables describing relations betwen codelists.
-#' This is especially useful for spatial hierarchies (which towns belong to which region), or for converting
+#' This is especially useful for spatial hierarchies (e.g. which towns belong to which region), or for converting
 #'  between categorisations (e.g. two different sets of IDs for regions.)
 #'
 #' You can pass a vector of two IDs (numeric or character) and if the relational
