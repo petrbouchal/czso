0cfdeaabc79803dbfcf2bbb0271a7ecbdd16ca8b
Petr Bouchal <pbouchal@gmail.com>
2020-07-23 13:00:51 +0200

    cleanup twitter example code

--- prep/sample-for-twitter.R
+++ prep/sample-for-twitter.R
@@ -2,20 +2,32 @@ library(czso)
 library(tidyverse)
 library(lubridate)
 
-czso_get_catalogue() %>% filter(str_detect(title, "onjunk")) %>% pull(page)
+czso_get_catalogue() %>%
+  filter(str_detect(title, "[Kk]onjunk"))
+
 konj <- czso_get_table("070013")
 
-konj2 <- konj %>% 
-  filter(is.na(bazobdobiod), obdobido > "2015-01-31", is.na(cznace_txt)) %>% 
-  mutate(obdobi_rok = year(obdobido), obdobi_mesic = month(obdobido))
-
-ggplot(konj2, aes(obdobi_mesic, hodnota, colour = obdobi_rok, group = obdobi_rok)) + 
+konj %>%
+  filter(is.na(bazobdobiod), obdobido > "2015-01-31", is.na(cznace_txt)) %>%
+  mutate(obdobi_rok = year(obdobido),
+         obdobi_mesic = as.integer(month(obdobido))) %>%
+  ggplot(aes(obdobi_mesic, hodnota, colour = obdobi_rok, group = obdobi_rok)) +
   geom_line() +
   geom_point() +
-  scale_color_viridis_c(direction = -1, guide = guide_legend(title = NULL)) +
   facet_wrap(~ stapro_txt) +
+  # ptrr::scale_x_number_cz() +
+  scale_color_viridis_c(direction = -1, guide = guide_legend(title = NULL)) +
   scale_x_continuous(n.breaks = 12) +
   ptrr::theme_ptrr("y", multiplot = T) +
+  labs(title = "Indikátory důvěry",
+       subtitle = "Konjunkturální průzkum, data ČSÚ",
+       caption = "Zdroj: ČSÚ, dataset '070013'. Sběr dat GfK pro ČSÚ")
+
+konj %>%
+  filter(is.na(bazobdobiod), obdobido > "2015-01-31", is.na(cznace_txt)) %>%
+  ggplot(aes(obdobido, hodnota, colour = stapro_txt)) +
+  geom_line() +
+  geom_point() +
   labs(title = "Indikátory důvěry",
        subtitle = "Konjunkturální průzkum, data ČSÚ",
        caption = "Zdroj: ČSÚ, dataset '070013'. Sběr dat GfK pro ČSÚ")
