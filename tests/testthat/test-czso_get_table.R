9a7fd5cda8ab95f5ec6a935d4ceb78939f10817a
Petr Bouchal <pbouchal@gmail.com>
2021-10-28 17:14:27 +0200

    update test logic + skip on CRAN to avoid fails when provider server unavailable

--- tests/testthat/test-czso_get_table.R
+++ tests/testthat/test-czso_get_table.R
@@ -1,9 +1,10 @@
-context("download works")
-
 # 100013 = Těžba dřeva podle druhů dřevin a typu nahodilé těžby
 
-expect_true(is.data.frame(czso_get_table("100013"))) # je data frame?
-expect_gte(nrow(czso_get_table("100013")), 399) # 399 = počet řádků k 2020-04-23
+test_that("download works", {
+  skip_on_cran()
+  expect_true(is.data.frame(czso_get_table("100013"))) # je data frame?
+  expect_gte(nrow(czso_get_table("100013")), 399) # 399 = počet řádků k 2020-04-23
 
-expect_true(is.data.frame(czso_get_codelist("cis100"))) # je data frame?
-expect_gte(nrow(czso_get_codelist("cis100")), 14) # 399 = počet řádků k 2020-04-23
+  expect_true(is.data.frame(czso_get_codelist("cis100"))) # je data frame?
+  expect_gte(nrow(czso_get_codelist("cis100")), 14) # 14 = počet sloupců k 2020-04-23
+})
