dfb2fdd81cba2ee98a9839db5150275a6811c325
Petr Bouchal <pbouchal@gmail.com>
2022-03-05 18:17:05 +0100

    first draft of codelist targets factory

--- R/targets.R
+++ R/targets.R
@@ -84,3 +84,82 @@ czso_target_table <- function(name, dataset_id, dest_dir, resource_num = 1, free
   )
 
 }
+
+czso_target_codelist <- function(name, codelist_id, language = c("cs", "en"),
+                                 dest_dir, resource_num = NULL, freeze = FALSE) {
+  if(!requireNamespace("targets", quietly = TRUE)) {
+    cli::cli_abort("This function requires the {.pkg targets} package.")
+  }
+
+  lng <- match.arg(language)
+
+  stopifnot(length(codelist_id) <= 2)
+
+  if(length(codelist_id) == 1) {
+    if(is.numeric(codelist_id) | grepl("^[0-9]{2,4}$", codelist_id)) {
+      codelist_id <- paste0("cis", codelist_id)
+    }
+  } else if(length(codelist_id) == 2) {
+    if(is.numeric(codelist_id) | all(grepl("^[0-9]{2,4}$", codelist_id))) {
+      codelist_id <- paste0("cis", codelist_id[1], "vaz", codelist_id[2])
+    }
+  }
+
+  if(!grepl("^cis", codelist_id)) {
+    cli::cli_alert_warning(c("The value you passed to {.var codelist_id} does not seem to indicate a codelist.",
+                             "This may cause unexpected results."))
+  }
+
+  cis_meta_orig <- get_czso_resources(codelist_id)
+  cis_meta_orig$dataset_id <- codelist_id
+
+  cis_meta <- cis_meta_orig[cis_meta_orig$format == "text/csv",]
+  cis_url <- cis_meta[cis_meta$format == "text/csv", "url"]
+
+  if(length(cis_url) < 1) {
+    # usethis::ui_stop(c("No CSV distribution for this codelist found.",
+    #                    "You can download the codelist in the format provided using {ui_code(x = stringr::str_glue('czso_get_table(\"{codelist_id}\")'))} and read it in manually.",
+    #                    "Use {ui_code(x = stringr::str_glue('czso_get_dataset_metadata(\"{codelist_id}\")'))} to see which formats are available."))
+
+    if(is.null(resource_num)) resource_num <- 1
+
+    cli::cli_inform("No documented CSV distribution found for this codelist. Using workaround.")
+
+    cis_meta <- cis_meta_orig[1,]
+    cis_url <- cis_meta[["url"]]
+    cis_url <- sub("format\\=0$", "format=2&separator=,", cis_url)
+    cis_meta$format <- "text/csv"
+    cis_meta$url <- cis_url
+  }
+
+  name_d <- deparse(substitute(name))
+  name_url <- paste0(name_d, "_url")
+  name_file <- paste0(name_d, "_file")
+  name_data <- paste0(name_d)
+  sym_file <- as.symbol(name_file)
+  sym_url <- as.symbol(name_url)
+  sym_data <- as.symbol(name_data)
+  sym_dest_dir <- as.symbol(dest_dir)
+
+  dfile <- get_dl_path(cis_meta, dest_dir)
+
+  url <- cis_url
+  if(lng == "en") cis_url <- sub("cisjaz=203", "cisjaz=8260", cis_url)
+
+  command_file <- substitute(get_file_for_targets(cis_url, cis_meta, dfile),
+                             env = list(url = sym_url, dfile = dfile,
+                                        cis_meta = cis_meta,
+                                        get_file_for_targets = get_file_for_targets))
+  command_data <- substitute(read_czso_csv(file),
+                             env = list(file = sym_file,
+                                        read_czso_csv = read_czso_csv))
+
+  list(
+    targets::tar_target_raw(name_url, url, format = "url", deployment = "main",
+                            cue = if(freeze) targets::tar_cue("never") else targets::tar_cue("thorough")
+    ),
+    targets::tar_target_raw(name_file, command_file, format = "file"),
+    targets::tar_target_raw(name_data, command_data)
+  )
+
+}
